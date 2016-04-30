f8db496499f3f3f6b92a43cc966975a93153563b
InfiniteYY <alan@fancypantsgroup.cn>
2016-04-30 16:35:54 +0800

    fix shallow clone in mergeObject func && add getNumber func

--- utils.js
+++ utils.js
@@ -5,11 +5,32 @@ define([],function(){
 	var utils = new Scheme();
 
 	proto.mergeObject = function(obj1,obj2){
+		var targetObj = {};
 		for(var key in obj2){
-			obj1[key] = obj2[key];
+			if(obj2[key] !== null && typeof obj2[key] === 'object'){
+				//nest object
+				if(obj1[key]){
+					targetObj[key] = this.mergeObject(obj1[key],obj2[key]);
+				}else{
+					targetObj[key] = obj2[key];
+				}
+			}else{
+				targetObj[key] = obj2[key];
+			}
 		}
 
-		return obj1;
+		return targetObj;
+	}
+
+	proto.getNumber = function(num,pos){
+		num = Math.floor(Math.abs(num));
+
+		if(num >= pos * 10){
+			var str_num = num.toString();
+			return str_num[str_num.length-pos-1].valueOf();
+		}else{
+			return null;
+		}
 	}
 
 	return utils;
